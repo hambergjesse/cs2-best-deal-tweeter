8c57ee9f2aa34374c571693666b4848c62f492fc
Jesse <hambergjesse@gmail.com>
2024-09-11 08:33:30 +0300

    blyat

--- manualRunner.js
+++ manualRunner.js
@@ -1,14 +1,5 @@
 import { getDailyBestDeal } from "./floatFetch.js";
 
-console.log("Twitter Client ID:", process.env.TWITTER_CLIENT_ID);
-console.log("Twitter Client Secret:", process.env.TWITTER_CLIENT_SECRET);
-console.log("Consumer Key:", process.env.CONSUMER_KEY);
-console.log("Consumer Secret:", process.env.CONSUMER_SECRET);
-console.log("Access Token:", process.env.ACCESS_TOKEN);
-console.log("Access Token Secret:", process.env.ACCESS_TOKEN_SECRET);
-console.log("Twitter Bearer Token:", process.env.TWITTER_BEARER_TOKEN);
-console.log("Float API Key:", process.env.FLOAT_API_KEY);
-
 async function manualProcess() {
   try {
     console.log("Running manual process...");
